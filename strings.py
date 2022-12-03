779a782cc1699617b340085ebbd533a018de1ec0
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-12-03 23:39:36 +0400

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,8 +1,8 @@
 TIMI_NEKO = (
- "Heyyo, timi wanna playyyy!!"
- "Why, Did you summon Timi!!!"
- "Timi Loves pats ^-^"
- "*Looks*"
- "Yeaa?"
+ "Heyyo, timi wanna playyyy!!",
+ "Why, Did you summon Timi!!!",
+ "Timi Loves pats ^-^",
+ "*Looks*",
+ "Yeaa?",
  ">~<"
 )
