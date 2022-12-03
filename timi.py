9238487cb9758317f140f1db5ecce640c01d565b
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-12-03 23:50:11 +0400

    Update timi.py

--- timi.py
+++ timi.py
@@ -33,6 +33,8 @@ print('Bot is Starting. Created By https://t.me/Sebastiansupport Devs. Timi is R
 
 ALIVE = ["https://telegra.ph/file/e39308158586bce4b9891.jpg", "https://telegra.ph/file/e9cce8b66270a4228fba6.jpg", "https://telegra.ph/file/f08c94883a79081b84255.jpg"]
 
+url_sfw = "https://api.waifu.pics/sfw/" 
+
 def get_command(comm: Union[list, str]):
   res = list()
   if isinstance(comm, str):
