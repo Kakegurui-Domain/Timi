7588e47cb57555a58826808a0bcd84dcf4a66f61
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-12-04 16:52:04 +0400

    Ok done

--- strings.py
+++ strings.py
@@ -9,6 +9,7 @@ TIMI_NEKO = (
 
 TIMI_HELP = """
 **Timi's Commands**
+• Say Timi(type Timi, timi) Timi will reply
 • Think : /think To Makes A Person Think
 • Pout : /pout To Makes A Person Pout
 • Facepalm : /facepalm To Makes A Person Facepalm
@@ -16,14 +17,9 @@ TIMI_HELP = """
 • Smug : /smug To Makes A Person Smug
 • Cry : /cry To Makes A Person Cry
 • Dance : /dance To Makes A Person Dance
-• Feed : /feed To Feed A Person
 • Shrug : /shrug To Shrug A Person
 • Bored : /bored To Makes A Person Bored
 • Pat : /pat To Pat A Person
 • Hug : /hug To Hug A Person
 • Slap : /slap To Slap A Person
-• Cute : /cute To Say Me Cute
-• Waifu : /waifu To Send Random Waifu Image
-• Kitsune : /kitsune To Send Random Kitsune Image
-• Sleep : /sleep To Say I Am Going To Sleep
 """
