0a85611e18a295c8d62e4a8efe6e20a8d955eda3
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-12-18 20:29:01 +0530

    Create cleanmode.py

---
+++ plugins/Database/cleanmode.py
@@ -0,0 +1,30 @@
+from timi import db
+
+cleandb = db.cleanmode
+cleanmode = {}
+
+
+async def is_cleanmode_on(chat_id: int) -> bool:
+    mode = cleanmode.get(chat_id)
+    if not mode:
+        user = await cleandb.find_one({"chat_id": chat_id})
+        if not user:
+            cleanmode[chat_id] = True
+            return True
+        cleanmode[chat_id] = False
+        return False
+    return mode
+
+
+async def cleanmode_on(chat_id: int):
+    cleanmode[chat_id] = True
+    user = await cleandb.find_one({"chat_id": chat_id})
+    if user:
+        return await cleandb.delete_one({"chat_id": chat_id})
+
+
+async def cleanmode_off(chat_id: int):
+    cleanmode[chat_id] = False
+    user = await cleandb.find_one({"chat_id": chat_id})
+    if not user:
+        return await cleandb.insert_one({"chat_id": chat_id})
