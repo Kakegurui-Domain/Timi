6d87dd22270b549ec436942c52999bdce525c543
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-12-21 15:11:10 +0530

    Update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -56,63 +56,6 @@ async def put_cleanmode(chat_id, message_id):
     cleanmode[chat_id].append(put)
 
 
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-# Copyright (C) 2022-2023 by theblacklinen@Github, < https://github.com/theblacklinen >.
-
-# This file is part of < https://github.com/theblacklinen/Erina-Ultra-Bot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/theblacklinen/Erina-Ultra-Bot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
-import time
-
-from pyrogram import filters
-from pyrogram.types import Message
-
-from Erina import bot as app
-from Erina.Database import add_afk, is_afk, remove_afk
-
-from typing import Union
-from datetime import datetime, timedelta
-
-def get_readable_time(seconds: int) -> str:
-    count = 0
-    ping_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
-    while count < 4:
-        count += 1
-        if count < 3:
-            remainder, result = divmod(seconds, 60)
-        else:
-            remainder, result = divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-    for i in range(len(time_list)):
-        time_list[i] = str(time_list[i]) + time_suffix_list[i]
-    if len(time_list) == 4:
-        ping_time += time_list.pop() + ", "
-    time_list.reverse()
-    ping_time += ":".join(time_list)
-    return ping_time
-
-cleanmode = {}
-
-async def put_cleanmode(chat_id, message_id):
-    if chat_id not in cleanmode:
-        cleanmode[chat_id] = []
-    time_now = datetime.now()
-    put = {
-        "msg_id": message_id,
-        "timer_after": time_now + timedelta(minutes=5),
-    }
-    cleanmode[chat_id].append(put)
-
 @app.on_message(filters.command(["afk", f"afk@TimiCuteBot"]))
 async def active_afk(_, message: Message):
     if message.sender_chat:
