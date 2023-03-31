d5947b21e4a870f52db20fc2535de605d031f615
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2023-03-31 06:09:24 +0530

    Test sub

--- plugins/chatbot.py
+++ plugins/chatbot.py
@@ -1,14 +1,16 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+
 from Timi import bot
 from pyrogram import*
 
-Timi = ChatBot("Timibot")
-Timi.set_trainer(ChatterBotCorpusTrainer)
-Timi.train("chatterbot.corpus.english")
+from transformers import pipeline
+
+
+timi = pipeline("text-generation", timi="gpt2")
+
 
 @bot.on_message(filters.text)
-def reply_message(bot, update):
-    user_text = update.message.text
-    response = str(english_bot.get_response(user_text))
-    bot.send_message(chat_id=update.message.chat_id, text=response)
+def handle_message(client, message):
+    response = timi(message.text, max_length=50, do_sample=True, temperature=0.7)[0]['generated_text']
+    bot.send_message(message.chat.id, response)
+
+
