3a1771e6390d82f46dea95ee9eef6e3dfa126b9f
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2023-03-31 06:31:27 +0530

    Update chatbot.py

--- plugins/chatbot.py
+++ plugins/chatbot.py
@@ -1,16 +1,64 @@
 
-from timi import bot
-from pyrogram import*
-
-from transformers import pipeline
+import pygame
+from pyrogram*
 
 
-timi = pipeline("text-generation", timi="gpt2")
+# Initialize Pygame
+pygame.init()
 
+# Set up Pygame display
+WINDOW_SIZE = (800, 600)
+screen = pygame.display.set_mode(WINDOW_SIZE)
 
-@bot.on_message(filters.text)
+# Set up player character
+player_image = pygame.image.load("player.png")
+player_rect = player_image.get_rect()
+
+# Set up enemy character
+enemy_image = pygame.image.load("enemy.png")
+enemy_rect = enemy_image.get_rect()
+
+# Set up game state
+player_hp = 100
+enemy_hp = 100
+is_battle = False
+
+# Create a message handler for incoming messages
+@ app.on_message(filters.text)
 def handle_message(client, message):
-    response = timi(message.text, max_length=50, do_sample=True, temperature=0.7)[0]['generated_text']
-    bot.send_message(message.chat.id, response)
+    global player_hp, enemy_hp, is_battle
+
+    # Handle user input
+    if message.text == "/up":
+        player_rect.move_ip(0, -10)
+    elif message.text == "/down":
+        player_rect.move_ip(0, 10)
+    elif message.text == "/left":
+        player_rect.move_ip(-10, 0)
+    elif message.text == "/right":
+        player_rect.move_ip(10, 0)
+    elif message.text == "/attack":
+        is_battle = True
+
+    # Handle game state
+    if is_battle:
+        enemy_hp -= 10
+        player_hp -= 5
+
+    # Draw game objects to screen
+    screen.fill((255, 255, 255))
+    screen.blit(player_image, player_rect)
+    screen.blit(enemy_image, enemy_rect)
+    pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(10, 10, player_hp, 10))
+    pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(600, 10, enemy_hp, 10))
+    pygame.display.flip()
 
 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            app.stop()
+            quit()
+
+    pygame.time.wait(10)
