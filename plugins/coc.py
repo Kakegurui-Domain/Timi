bc3e7befe371bc1c33133cf2cc94c3fad5c62b53
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2023-04-01 12:47:03 +0530

    Update coc.py

--- plugins/coc.py
+++ plugins/coc.py
@@ -2,6 +2,7 @@
 
 from pyrogram import*
 from timi import bot
+import requests
 
 @bot.on_message(filters.command('player'))
 def get_player_info(_, message):
@@ -13,7 +14,8 @@ def get_player_info(_, message):
 @bot.on_message(filters.command('clan'))
 def get_clan_info(client, message):
     clan_tag = message.text.split()[1]
-    data = response.json()
+    tf = requests.get(f'https://api.clashofclans.com/v1/players/{clan_tag}', headers={'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjM1NDMwMzdmLTBjODMtNDBkNC1iNGVhLTE4YjA0ZTU2ZGUwNyIsImlhdCI6MTY4MDMzMjE4OCwic3ViIjoiZGV2ZWxvcGVyLzJmYmVmZGJmLTg2MWUtY2RlOS0wY2M2LTE1ZGMwNjZhMDYxNSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEzNy45Ny43OC41MSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.t64BgE-XsZM3nBHm7qXogQ8Q4hcHPemALYlyM8CLbaw6cC671iEHnZd40KRyCDXe7jLEAC6BKSaodUh5NKclDg'})
+    dataa = tf.json()
     message.reply_text(f"information about the clan: {info}")
 
 
